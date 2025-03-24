f9c7bcbbbb3f55fb03818f5a03cf7ef02b369481
Abraham Samuel <samuak2004@gmail.com>
2025-03-25 01:09:53 +0530

    Update code_for_face_without_pi.ino

--- Face_attendanace/code_for_face_without_pi/code_for_face_without_pi.ino
+++ Face_attendanace/code_for_face_without_pi/code_for_face_without_pi.ino
@@ -2,7 +2,6 @@
 #include <LiquidCrystal_I2C.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <RTClib.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
@@ -10,8 +9,7 @@
 #define LED 6
 
 MFRC522 rfid(SS_PIN, RST_PIN);
-RTC_DS3231 rtc;
-LiquidCrystal_I2C lcd(0x27, 16, 2); // LCD with I2C
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
     Serial.begin(9600);
@@ -19,13 +17,9 @@ void setup() {
     rfid.PCD_Init();
     lcd.begin(16, 2);
     lcd.backlight();
+    
     pinMode(BUZZER, OUTPUT);
     pinMode(LED, OUTPUT);
-    
-    if (!rtc.begin()) {
-        Serial.println("Couldn't find RTC");
-        while (1);
-    }
     
     lcd.setCursor(0, 0);
     lcd.print("Scan your card");
@@ -36,26 +30,36 @@ void loop() {
         return;
     }
 
-    String cardID = "";
+    // Convert RFID data to a string
+    String cardData = "";
     for (byte i = 0; i < rfid.uid.size; i++) {
-        cardID += String(rfid.uid.uidByte[i], HEX);
+        cardData += String(rfid.uid.uidByte[i], HEX);
     }
 
-    Serial.println(cardID);  // Send RFID ID to Python
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Face Verify...");
 
-    // Wait for response from Python
-    while (!Serial.available());
+    Serial.println(cardData); // Send RFID to Python
+
+    while (!Serial.available()); // Wait for Python response
 
     String response = Serial.readStringUntil('\n');
     response.trim();
 
     if (response != "Unauthorized") {
         lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Welcome, " + response);
+       // Split response into Name and Roll Number
+      int commaIndex = response.indexOf(',');
+      String name = response.substring(0, commaIndex);    // Extract name
+      String rollNo = response.substring(commaIndex + 1); // Extract roll number
+
+      lcd.setCursor(0, 0); // Top line
+      lcd.print(name);      // Print name
+  
+      lcd.setCursor(0, 1); // Second line
+      lcd.print(rollNo);    // Print roll number
+
         digitalWrite(LED, HIGH);
         digitalWrite(BUZZER, HIGH);
         delay(500);
